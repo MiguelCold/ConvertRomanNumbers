e3623026a9196134d5ce504980a20ab3e7b3426b
Miguel Ángel Ossa Ruiz <miguelcold8@gmail.com>
2014-11-26 08:45:28 -0500

    Creating the Converter Roman Numbers proyect in java using Eclipse.

---
+++ ConvertRomanNumbers/src/co/edu/udea/convertromannumbers/process/Converter.java
@@ -0,0 +1,5 @@
+package co.edu.udea.convertromannumbers.process;
+
+public class Converter {
+
+}
---
+++ ConvertRomanNumbers/test/co/edu/udea/convertromannumbers/test/process/TestConverter.java
@@ -0,0 +1,5 @@
+package co.edu.udea.convertromannumbers.test.process;
+
+public class TestConverter {
+
+}
